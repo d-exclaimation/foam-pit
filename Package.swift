77c7221cd72894bd69b1c1357de806a51796b1f0
Vincent <thisoneis4business@gmail.com>
2023-02-16 20:41:06 +1300

    chore: Format

--- Package.swift
+++ Package.swift
@@ -6,16 +6,17 @@ import PackageDescription
 let package = Package(
     name: "FoatPit",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/vapor", from: "4.67.1"),
         .package(url: "https://github.com/d-exclaimation/pioneer", from: "1.0.0"),
+        // .package(path: "../../../libs/pioneer"),
         .package(url: "https://github.com/GraphQLSwift/Graphiti", from: "1.2.0"),
         .package(url: "https://github.com/GraphQLSwift/GraphQL", from: "2.4.3"),
-        .package(url: "https://github.com/jordanbaird/Prism", from: "0.0.9")
+        .package(url: "https://github.com/jordanbaird/Prism", from: "0.0.9"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,7 +28,8 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Graphiti", package: "Graphiti"),
                 .product(name: "GraphQL", package: "GraphQL"),
-                .product(name: "Prism", package: "Prism")
-            ]),
+                .product(name: "Prism", package: "Prism"),
+            ]
+        ),
     ]
 )
